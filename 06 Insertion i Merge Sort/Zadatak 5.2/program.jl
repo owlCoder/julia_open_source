dfe04b5109f54c6c717a7a62211b7e5a72c349b4
Danijel Jovanović <danijel.xda@gmail.com>
2021-04-28 22:46:04 +0200

    Zadatak 5.2

---
+++ 06 Insertion i Merge Sort/Zadatak 5.2/program.jl
@@ -0,0 +1,30 @@
+function mergeSort!(A)
+   mergeSortStep!(A, 1, length(A));
+end
+
+function mergeSortStep!(A, p, r)
+   if p < r
+      q = Int(floor((p+r)/2));
+      mergeSortStep!(A, p, q);
+      mergeSortStep!(A, q+1, r);
+      merge!(A, p, q, r);
+   end
+end
+
+function merge!(A, p, q, r)
+	L = copy(A[p:q])
+	R = copy(A[q+1:r])
+	push!(L, typemin(Int64))
+	push!(R, typemin(Int64))
+	i = 1;
+	j = 1;
+	for k = p:r
+		if L[i] >= R[j]
+			A[k] = L[i];
+			i = i + 1;
+		else
+			A[k] = R[j];
+			j = j + 1;
+		end
+	end
+end
