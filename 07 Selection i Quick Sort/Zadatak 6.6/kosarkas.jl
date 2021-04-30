cfde0704e566b30347aa1344dc1403e7ed8cc22c
Danijel Jovanović <danijel.xda@gmail.com>
2021-04-30 15:11:43 +0200

    Zadatak 6.6

--- 07 Selection i Quick Sort/Zadatak 6.6/kosarkas.jl
+++ 07 Selection i Quick Sort/Zadatak 6.6/kosarkas.jl
@@ -15,11 +15,13 @@ push!(košarkaši, Košarkaš("Napoli", "Tutlo",      134, "levo krilo"))
 push!(košarkaši, Košarkaš("Inter", "Kamalo",      127, "desno krilo"))
 push!(košarkaši, Košarkaš("Roma", "Divergeni",    144, "centar"))
 push!(košarkaši, Košarkaš("Fiorentina", "Tiruto", 149, "bek"))
-push!(košarkaši, Košarkaš("Lazio", "Kamalo",      138,  "centar"))
+push!(košarkaši, Košarkaš("Lazio", "Kamalo",      138, "centar"))
 push!(košarkaši, Košarkaš("Atalanta", "Kamalo",   138, "levo krilo"))
 
 function uporedi(K1, K2)
-	return K1.visina > K2.visina || K1.pozicija == K2.pozicija
+	return K1.visina > K2.visina || 
+        (K1.visina == K2.visina && K1.pozicija > K2.pozicija)
+end
 
 function ispisKošarkaša(košarkaši)
   for i in 1:length(košarkaši)
--- 07 Selection i Quick Sort/Zadatak 6.6/poziv.jl
+++ 07 Selection i Quick Sort/Zadatak 6.6/poziv.jl
@@ -1,3 +1,31 @@
 # Zadatak 6.6. Sortirati niz košarkaša koristeći Selection sort algoritam rastuće po
 # visini. Košarkaše sa istom visinom sortirati opadajuće po poziciji na kojoj igraju.
 # Ispisati razliku u visini između najvišeg i najnižeg košarkaša
+
+include("kosarkas.jl")
+include("selectionSort.jl")
+
+println("Nesortirani spisak košarkaša:")
+ispisKošarkaša(košarkaši)
+println()
+
+selectionSort!(košarkaši)
+println("Sortirani spisak košarkaša:")
+ispisKošarkaša(košarkaši)
+
+visine = []
+for i in 1:length(košarkaši)
+    push!(visine, košarkaši[i].visina)
+end
+
+razlika = maximum(visine) - minimum(visine)
+d1, najvisi = findmax(visine)
+d2, najnizi = findmin(visine)
+
+println("\nNAJNIŽI KOŠARKAŠ")
+@printf("%2i. %-15s %-15s %3i\n", najnizi, košarkaši[najnizi].ime, košarkaši[najnizi].prezime, košarkaši[najnizi].visina)
+
+println("\nNAJVIŠI KOŠARKAŠ")
+@printf("%2i. %-15s %-15s %3i\n", najvisi, košarkaši[najvisi].ime, košarkaši[najvisi].prezime, košarkaši[najvisi].visina)
+
+println("\nRazlika u visini iznosi: $razlika cm.")
