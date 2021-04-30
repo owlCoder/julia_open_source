077da4eaa812d32c733453e5f655639a9889341a
Danijel Jovanović <danijel.xda@gmail.com>
2021-04-30 14:56:39 +0200

    Zadatak 6.5

--- 07 Selection i Quick Sort/Zadatak 6.5/poziv.jl
+++ 07 Selection i Quick Sort/Zadatak 6.5/poziv.jl
@@ -1,4 +1,18 @@
 # Zadatak 6.5. Sortirati reči u rečniku upotrebom Selection sort algoritma. Reči su
 # zadate kao niz stringova. Nakon toga, prebrojati koje reči se najviše puta pojavljuju.
 
-include("selectionSort.jl")
+# KOREKCIJA: KORISTI SE QUICK SORT ALGORITAM JER Selection Sort ne radi!
+include("recnik.jl")
+# include("selectionSort.jl")
+include("quickSort.jl")
+include("pretragaPojava.jl")
+
+println("Rečnik pre sortiranja")
+ispisRečnika(recnici)
+
+quickSort!(recnici)
+println("\nRečnik posle sortiranja")
+ispisRečnika(recnici)
+
+elementiPojave, brojPojava = pretraga(recnici)
+println("\nPretraga pojava reči najveći broj puta...\n>> $elementiPojave\n>> ponovljena $brojPojava puta...\n")
--- 07 Selection i Quick Sort/Zadatak 6.5/selectionSort.jl
+++ 07 Selection i Quick Sort/Zadatak 6.5/selectionSort.jl
@@ -1,1 +1,18 @@
-# code here
+# Selection sort ne sortira lepo niz, dok quickSort radi korektno
+function selectionSort!(recnici)
+    n = length(recnici)
+
+    for i in 1:(n - 1)
+        minIndeks = i
+
+        for j in (i + 1):n
+            if uporedi(recnici[j], recnici[i])
+                minIndeks = j
+            end
+        end
+
+        if i != minIndeks
+            recnici[[i minIndeks]] = recnici[[minIndeks i]]
+        end
+    end
+end
