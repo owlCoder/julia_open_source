6a9b2a1a448dfe9afeddf187ad102e7f279a9439
Danijel Jovanović <danijel.xda@gmail.com>
2021-04-30 15:22:07 +0200

    Zadatak 6.7

--- 07 Selection i Quick Sort/Zadatak 6.7/poziv.jl
+++ 07 Selection i Quick Sort/Zadatak 6.7/poziv.jl
@@ -1,3 +1,17 @@
 # Zadatak 6.7. Sortirati brojeve niza A opadajuće Quick sort algoritmom i za svaki broj
 # iz niza B ispisati da li se nalazi u prvoj ili drugoj polovini sortiranog niza.
 
+include("quickSort.jl")
+
+A = [2 5 19 7 14 9 1 22 8]
+
+quickSort!(A)
+println("Sortirani niz izgleda ovako: $A\n")
+
+B = copy(A)
+
+polovina = Int(floor(length(B) / 2))
+
+for i in 1:length(B)
+    println("$(B[i])\t $(i <= polovina ? "PRVA POLOVINA" : "DRUGA POLOVINA")")
+end
--- 07 Selection i Quick Sort/Zadatak 6.7/quickSort.jl
+++ 07 Selection i Quick Sort/Zadatak 6.7/quickSort.jl
@@ -1,1 +1,31 @@
-# code here
+function quickSort!(A)
+    quickSortStep!(A, 1, length(A))
+end
+
+function quickSortStep!(A, p, r)
+    if p < r
+        q = partition!(A, p, r)
+        quickSortStep!(A, p, q-1)
+        quickSortStep!(A, q+1, r)
+    end
+end
+
+function uporedi(r1, r2)
+    return r1 < r2
+end 
+
+function partition!(A, p, r)
+    x = A[r];
+    i = p - 1;
+
+    for j = p:r-1
+        if uporedi(A[j], x)
+            i = i + 1;
+            A[[i j]] = A[[j i]];
+        end
+    end
+
+    A[[i+1 r]] = A[[r i+1]];
+    
+    return i + 1;
+end 
