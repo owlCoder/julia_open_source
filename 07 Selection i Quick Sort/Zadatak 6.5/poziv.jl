0ad9ab0f9b59a9e9ac579718a7d1dac0ac64b700
Danijel Jovanović <jovanovic.pr55.2020@uns.ac.rs>
2021-05-08 11:54:01 +0200

    Clean up outdated note

--- 07 Selection i Quick Sort/Zadatak 6.5/poziv.jl
+++ 07 Selection i Quick Sort/Zadatak 6.5/poziv.jl
@@ -1,7 +1,6 @@
 # Zadatak 6.5. Sortirati reči u rečniku upotrebom Selection sort algoritma. Reči su
 # zadate kao niz stringova. Nakon toga, prebrojati koje reči se najviše puta pojavljuju.
 
-# KOREKCIJA: KORISTI SE QUICK SORT ALGORITAM JER Selection Sort ne radi!
 include("recnik.jl")
 include("selectionSort.jl")
 include("pretragaPojava.jl")
