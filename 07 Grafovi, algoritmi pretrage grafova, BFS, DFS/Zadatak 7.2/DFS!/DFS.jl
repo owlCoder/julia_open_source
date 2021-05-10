797518cf1ca99f1be50185155d240be29adde98b
Danijel Jovanović <danijel.xda@gmail.com>
2021-05-10 18:39:30 +0200

    Zadatak 7.2

--- 07 Grafovi, algoritmi pretrage grafova, BFS, DFS/Zadatak 7.2/DFS!/DFS.jl
+++ 07 Grafovi, algoritmi pretrage grafova, BFS, DFS/Zadatak 7.2/DFS!/DFS.jl
@@ -5,21 +5,23 @@ function DFS!(Graf)
         Graf.V[u].boja = 'W'
         Graf.V[u].prethodni = -1
     end
+    rbOstrva = 0
 
     for u = v
         if G.V[u].boja == 'W'
-            DFS_Poseti!(Graf, u)
+            rbOstrva = rbOstrva + 1
+            DFS_Poseti!(Graf, u, rbOstrva)
         end
     end
 end
 
-function DFS_Poseti!(Graf, u)
+function DFS_Poseti!(Graf, u, rbOstrva)
     Graf.V[u].boja = 'G'
-
+    Graf.V[u].udaljenost = rbOstrva # d umesto za razdaljinu, koristimo pripadnost ostrvu
+    
     for v in findall(Graf.AdjMatrix[u, :] .== 1)
         if Graf.V[v].boja == 'W'
-            G.V[v].prethodni = u
-            DFS_Poseti!(Graf, v)
+            DFS_Poseti!(Graf, v, rbOstrva)
         end
     end
     G.V[u].boja = 'B'
