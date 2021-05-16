e84559c2cfae384cecaabe5d08512e4ce73af9bf
Danijel Jovanović <danijel.xda@gmail.com>
2021-05-16 13:27:59 +0200

    Rework BFS Recursive algorithm

--- 08 Grafovi, BFS, DFS, lavirint/Zadatak 8.3/BFS_rec.jl
+++ 08 Grafovi, BFS, DFS, lavirint/Zadatak 8.3/BFS_rec.jl
@@ -4,33 +4,33 @@ polja do kojih je moguće doći počevši od bilo kojeg elementa te sobe.
 – Ideja: Pogledati zadatak 7.3. Kreirati funkcije BFS i BFS_visit, pri čemu
 BFS vrši inicijalizaciju i pokreće po jednom BFS_visit za svaku sobu. ==#
 
-include("graph.jl")
-
-function BFS!(Graf, početak, kraj)
+function BFS!(Graf)
     v = 1:size(Graf.AdjMatrix, 1)
     
     for u = v
-        Graf.V[u].boja = 'W'
-        Graf.V[u].udaljenost = typemax(Int64)
-        Graf.V[u].prethodni = -1
+        Graf.V[u].color = 'W'
+        Graf.V[u].pred = -1
     end
 
-    DFS_Poseti!(Graf, početak, kraj, početak)
-end
+    rbOstrva = 0
 
-function DFS_Poseti!(Graf, u, kraj, početak)
-    if u == kraj
-        putanja = pronađiPutanju(Graf, početak, kraj)
-        println("Put: $putanja")
-    else
-        Graf.V[u].boja = 'G'
-
-        for v in findall(Graf.AdjMatrix[u, :] .== 1)
-            if Graf.V[v].boja == 'W'
-                G.V[v].prethodni = u
-                DFS_Poseti!(Graf, v, kraj, početak)
-            end
+    for u = v
+        if Graf.V[u].color == 'W' && Graf.V[u].type == 'P' && Graf.V[u].pred <= 1   
+            rbOstrva += 1
+            BFS_Visit!(G, u, rbOstrva)
         end
-        G.V[u].boja = 'W'
     end
 end
+
+function BFS_Visit!(Graf, u, rbOstrva)
+    G.V[u].d = rbOstrva    
+    Graf.V[u].color = 'G'
+
+    for v in findall(Graf.AdjMatrix[u, :] .== 1)
+        if Graf.V[v].color == 'W' && Graf.V[v].type == 'P'
+            BFS_Visit!(Graf, v, rbOstrva)
+        end
+    end
+    G.V[u].color = 'B'
+end
+
